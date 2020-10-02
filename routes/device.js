ab82d5ed882c15475e672211b1e589879cead518
victor77dev <victor77.dev@gmail.com>
2020-10-02 13:15:01 +0200

    Add report log route

--- routes/device.js
+++ routes/device.js
@@ -17,4 +17,18 @@ router.get('/read/:serial/:token', function(req, res, next) {
   });
 });
 
+/* POST report device log. */
+router.post('/report/:serial/:token', function(req, res, next) {
+  const {
+    serial,
+    token,
+  } = req.params;
+
+  console.log(serial, token)
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.send(200);
+});
+
 module.exports = router;
